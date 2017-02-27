5d9d91e36a063e4a195949c146470c65069c28b8
Tyrel Rucker <trucker@wlapcs.org>
2017-02-27 18:55:09 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function clearDisplay(){
+    document.getElementById("display-bar").innerHTML = "";
+}
+
+function display(userInput){
+     document.getElementById("display-bar").innerHTML += userInput;
+}
+
+
+function calculate(){
+    var answer = eval(document.getElementById("display-bar").innerHTML);
+  document.getElementById("display-bar").innerHTML = answer; 
+    
+}
